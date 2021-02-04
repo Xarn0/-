ecf127d2ef92b0e2e4658fd408946b10bbdc43df
dreamerad <56167606+dreamerad@users.noreply.github.com>
2021-02-04 14:45:29 +0600

    коментарии index commit

--- index.php
+++ index.php
@@ -8,15 +8,15 @@ $set['title']='Заголовок';
 
 // include_once 'sys/functions.php';
 
-include_once 'inc/head.php';
+include_once 'inc/head.php'; //Структура html
 
 if(isset($res))
 {
-include_once 'shop.php';
+include_once 'shop.php'; //Если человек авторизировался
 }
 if(!isset($res))
 {
-include_once 'views/welcom.php';
+include_once 'views/welcom.php'; //Если человек не авторизировался
 }
 
-// include_once 'inc/uhead.php';
+// include_once 'inc/uhead.php'; //Уведомления
